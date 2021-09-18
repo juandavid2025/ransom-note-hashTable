49855464c57fbb0ec5b5b3ce14d381ae21ec625a
juandavid2025 <juandavidaguirre2025@gmail.com>
2021-09-18 02:58:58 -0500

    updated version of the solution, adding comments to the classes

--- ransomNote/src/main/Main.java
+++ ransomNote/src/main/Main.java
@@ -1,24 +1,80 @@
 package main;
 
+import java.util.ArrayList;
+
 import ransomNote.RansomNote;
 
+/**
+ * Main class
+ * @author juandavid2025
+ */
 public class Main {
  
-	private RansomNote ran;
+	/**
+	 * Association with the class RansomNote in package ransomNote
+	 */
+	private RansomNote ransom;
 	
+	/**
+	 * Main constructor
+	 */
 	public Main() {
-	 ran=new RansomNote();
-	
-	 String k="hola 8 8 8 8 8 8 8 8 8 8 ";
-	 String L="hola xd y u";
-	 
-	 String[] magazine = k.split(" ");
-	 String[] note= L.split(" ");
-	 
-	 ran.checkMagazine(magazine, note);
-	
+		ransom = new RansomNote();
+		
+		System.out.print(runTests());
 	}
 	
+	/**
+	 * Set up and run some tests to verify if the solution is correct
+	 * @return true - if all test were successful, false - if at least one failed
+	 */
+	public boolean runTests() {
+		
+		boolean allCorrect = true;
+		
+		ArrayList<String> magazinesTestArrayList = new ArrayList<String>();
+		ArrayList<String> notesTestArrayList = new ArrayList<String>();
+		ArrayList<Boolean> expectedResult = new ArrayList<Boolean>();
+		setUpArrayListTests(magazinesTestArrayList, notesTestArrayList, expectedResult);
+		
+		for(int i=0; i<magazinesTestArrayList.size() && allCorrect; i++) {
+			
+			boolean result = ransom.isNoteInMagazine(magazinesTestArrayList.get(i), notesTestArrayList.get(i));
+			
+			if(!(result==expectedResult.get(i))) {
+				allCorrect=false;
+				System.out.print("\n Failed test #" + i +"\n");
+			}
+			
+		}
+		
+		return allCorrect;
+	}
+	
+	/**
+	 * Set up some tests in the ArrayLists to verify if the solution is correct
+	 * @param magazines - magazines ArrayList to add tests
+	 * @param notes - notes ArrayList to add tests
+	 * @param expected - expected result ArrayList to contrast tests
+	 */
+	public void setUpArrayListTests(ArrayList<String> magazines, ArrayList<String> notes, ArrayList<Boolean> expected) {
+		magazines.add("give me one grand today night");
+		notes.add("give one grand today");
+		expected.add(true);
+		
+		magazines.add("two times three is not four");
+		notes.add("two times two is four");
+		expected.add(false);
+		
+		magazines.add("ive got a lovely bunch of coconuts");
+		notes.add("ive got some coconuts");
+		expected.add(false);
+	}
+	
+	/**
+	 * Main method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		Main m = new Main();
--- ransomNote/src/ransomNote/RansomNote.java
+++ ransomNote/src/ransomNote/RansomNote.java
@@ -1,162 +1,211 @@
 package ransomNote;
 
+/**
+ * The RansomNote class will contain the hash table (String array) and the logic to search if the note can be written with the magazine alphabetic characters (an alphabetic character cannot be used more than once) 
+ * @author juandavid2025
+ */
 public class RansomNote { 
 	
-	public void checkMagazine(String[]magazine,String[]note) {
-		int tableSize=magazine.length*2;
-		String [] magazineTable = new String[tableSize];
-		int place;
+	/**
+	 * hastTable (list) that will contain each splitted fragment of the magazine
+	 */
+	private String [] hashTable;
+	
+	/**
+	 * List that will contain, in the same position as it's character in hashTable, the amount of times we can use that specific fragment
+	 */
+	private Integer [] alphabeticRepeatHelper;
+	
+	/**
+	 * Main method to be call to verify if a note can be done with magazine words, this method will organize the execution order of the others methods
+	 * @param magazine - magazine text
+	 * @param note - note text
+	 */
+	public boolean isNoteInMagazine(String magazine, String note) {
 		
-		// pasar el magazine a una tabla hash
-		for(int i =0;i<magazine.length;i++) {
-			place=hashValue(magazine[i],tableSize);
-			// si el lugar esta vacio----
-			if(magazineTable[place]==null) {
-				magazineTable[place]=magazine[i];
-			}
-			//si no lo esta COLISION----
-			else {
-				
-				//metodo de Quadratic Hashing
-				//quadraticHashing(place+1,magazine[i],tableSize,magazineTable);
-				linealHashing(place+1,magazine[i],tableSize,magazineTable);
-			}
-			
-		}
-		// fin de pasar el magazine a tabla hash
-		boolean completa=true;
-		for(int i=0;i<note.length && completa;i++) {
-			
-			if(!buscarValorLineal(note[i],magazineTable,tableSize)) {
-				completa=false;
-			}
-			
-		}
-		// valor si esta o no completa la note en las palabras del magazine
+		placingMagazineHashTable(magazine);
 		
-		System.out.println(completa);
+		//printHashTable();
+		
+		return verifyNoteInHashTable(note);
 	}
 	
-	public int hashValue(String toHash,int tableSize) {
+	/**
+	 * This method will organize the string magazine into the hashTable (initializing the class attributes in the process). To complete this it will be calling other methods: hashValue and linearHashing
+	 * @param magazine - magazine text
+	 */
+	public void placingMagazineHashTable(String magazine) {
+		
+		String [] magazineSplitted = magazine.split(" ");
+	
+		hashTable = new String[magazineSplitted.length*2];
+		alphabeticRepeatHelper = new Integer[magazineSplitted.length*2];
+		
+		int placement;
+		
+		// place all magazine characters in hash table
+		for(int i =0;i<magazineSplitted.length;i++) {
+			
+			placement=hashValue(magazineSplitted[i]);
+			
+			// if the placement generated in the hashTable is empty
+			if(hashTable[placement]==null) {
+				hashTable[placement] = magazineSplitted[i];
+				alphabeticRepeatHelper[placement] = 1;
+			}
+			
+			// if the placement in the hashTable is not empty it means we have a collision
+			else {
+				linealHashing(placement, magazineSplitted[i]);
+			}
+			
+		}	
+	}
+	
+	/**
+	 * This method is the one that will contrast the note with the magazine in the hashTable, so this method should be called after the process of placingMagazineHashTable
+	 * @param note - note text
+	 * @return boolean isNote that will be true if the note can be written with the words in magazine
+	 */
+	public boolean verifyNoteInHashTable(String note){
+		boolean isNote = true;
+		String[] noteSplitted = note.split(" ");
+		
+		for(int i=0;i<noteSplitted.length && isNote;i++) {
+			
+			if(!searchLinearForm(noteSplitted[i])) {
+				isNote=false;
+			}
+			
+		}
+		
+		return isNote;
+	}
+	
+	/**
+	 * Method that will return the hashValue of a String calling a method to get the sum of all characters of the String to later apply % with the length of the hashTable to ensure that the value is not out of bounds with the table
+	 * @param toHash - String which we want to get the hashValue
+	 * @return hash - hash final result of the addition
+	 */
+	public int hashValue(String toHash) {
 		int hash=0;
 		
-		hash = StringToCharSum(toHash)%tableSize;
+		hash = (StringToCharSum(toHash))%(hashTable.length);
 		
 		return hash;
 	}
 	
-	public int StringToCharSum(String cadena) {
-		int value=0;
+	/**
+	 * Method that will return the addition of chart characters of the param String
+	 * @param text - text
+	 * @return final value of the sum
+	 */
+	public int StringToCharSum(String text) {
+		int total=0;
 		
-		for(int i=0;i<cadena.length();i++) {
-			value+=cadena.charAt(i);
+		for(int i=0;i<text.length();i++) {
+			total += text.charAt(i);
 		}
 		
-		return value;
+		return total;
 	}
 	
-	public void quadraticHashing(int place,String value,int tableSize,String[] magazineTable) {
+	/**
+	 * this method will search if an specific word that is passed as param is on the hashTable with the linear approach
+	 * @param word
+	 * @return true when it was found, false when it was not found
+	 */
+	public boolean searchLinearForm(String word) {
+		boolean isThere=false;
 		
-		int realPlace=realPlace(place,tableSize);
-		boolean insertado=false;
+		int placeGenerated = hashValue(word);
 		
-		do {
+		if(hashTable[placeGenerated] != null) {
 			
-			if(magazineTable[realPlace(realPlace,tableSize)]==null) {
-				magazineTable[realPlace]=value;
-				insertado=true;
-			}else {
-				
-				realPlace=realPlace(realPlace*realPlace,tableSize);
+			if(hashTable[placeGenerated].equals(word)) {
+				if(alphabeticRepeatHelper[placeGenerated] == 1) {
+					hashTable[placeGenerated]=null;
+					alphabeticRepeatHelper[placeGenerated] = 0;
+					isThere = true;
+				}
+				else {
+					alphabeticRepeatHelper[placeGenerated]-=1;
+					isThere = true;
+				}
 			}
-			
-		}while(!insertado);
-	
-		
-	}
-	
-	public int realPlace(int place,int tableSize) {
-		int realPlace=place;
-		
-		
-		if(place>=tableSize) {
-			
-			do {
-			realPlace=realPlace-tableSize;
-			
-			}
-			while(realPlace>=tableSize);	
-		}
-		
-		
-		return realPlace;
-	}
-	
-	public boolean buscarValorLineal(String valorNote,String[]magazine,int tableSize) {
-		boolean esta=false;
-		if(magazine[hashValue(valorNote,tableSize)]!=null) {
-			if(magazine[hashValue(valorNote,tableSize)].equals(valorNote)) {
-			esta=true;
-		 }
 			else {
-				buscarValorLinealR(valorNote,magazine,tableSize,hashValue(valorNote,tableSize));
-		 }
-		}
-		return esta;
-	}
-	
-	public boolean buscarValorLinealR(String valorNote,String[]magazine,int tableSize,int posicion) {
-		
-		if(magazine[posicion]!=null) {
-			
-		     if(magazine[posicion].equals(valorNote)) {
-				return true;
-			}else {
-				return buscarValorLinealR(valorNote,magazine,tableSize,realPlace(posicion+1,tableSize));
+				if(placeGenerated>=hashTable.length) {
+					placeGenerated=placeGenerated-hashTable.length;
+				}
+				isThere = searchLinearFormR(placeGenerated+1, word);
 			}
-		}else {
-			return false;
 		}
+	
 		
+		return isThere;
 	}
 	
-	public boolean buscarValor(String valorNote,String[]magazine,int tableSize) {
-		boolean esta=false;
+	/**
+	 * complementary method of searchLinearForm that will help us with recursion
+	 * @param place - place in the hashTable that we are now
+	 * @param value - text that we are searching
+	 * @return true if it is in the hashTable, false if not
+	 */
+	public boolean searchLinearFormR(int place, String value) {
 		
-		if(magazine[hashValue(valorNote,tableSize)].equals(valorNote)) {
-			esta=true;
+		if(hashTable[place] != null) {
+			if(hashTable[place].equals(value)) {
+				if(alphabeticRepeatHelper[place] == 1) {
+					hashTable[place]=null;
+					alphabeticRepeatHelper[place] = 0;
+					return true;
+				}
+				else {
+					alphabeticRepeatHelper[place]-=1;
+					return true;
+				}
+			}
+			else {
+				if(place>=hashTable.length) {
+					place=place-hashTable.length;
+				}
+				return searchLinearFormR(place+1, value);
+			}
 		}
 		else {
-			esta=buscarValorR(valorNote,magazine,tableSize,realPlace(hashValue(valorNote,tableSize)+1,tableSize));
+			return false;
 		}
-		
-		return esta;
 	}
 	
-	// forma recursiva
-	public boolean buscarValorR(String valorNote,String[]magazine,int tableSize,int posicion) {
-	
-		if(magazine[posicion]!=null) {
-	
-	     if(magazine[posicion].equals(valorNote)) {
-			return true;
-		}else {
-			return buscarValorR(valorNote,magazine,tableSize,realPlace(posicion*posicion,tableSize));
+	/**
+	 * method that use linear hashing (if an space in the hashMap is already taken search in the next space) with the little modification that if the position is taken for the same word as current, it will add a counter in alphabeticRepeatHelper at that position
+	 * @param place - place in the hashTable that we are now
+	 * @param value - text that we are placing
+	 */
+	public void linealHashing(int place,String value) {
+		
+		if(hashTable[place]==null) {
+			hashTable[place]=value;
+			alphabeticRepeatHelper[place] = 1;
 		}
-	}else {
-		return false;
+		else if(hashTable[place] == value) {
+			alphabeticRepeatHelper[place] += 1;
+		}
+		else {
+			linealHashing(place+1, value);
+		}
 	}
-  }
 	
-	public void linealHashing(int place,String value,int tableSize,String[]magazine) {
+	/**
+	 * method to print the table (debug option)
+	 */
+	public void printHashTable() {
+		String representation = "";
 		
-		int realPlace=realPlace(place,tableSize);
-		
-		if(magazine[realPlace]==null) {
-			magazine[realPlace]=value;
-		}else {
-			linealHashing(place+1,value,tableSize,magazine);
+		for(int i=0;i<hashTable.length;i++) {
+			representation += "["+ hashTable[i] +"]"; 
 		}
 		
+		System.out.print(representation);
 	}
 }
